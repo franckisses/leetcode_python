825f60bd1316096484f5b42dd0d70f45802094b1
yangong1 <franck_gxu@outlook.com>
2020-09-27 17:12:51 +0200

    tests

---
+++ Check_If_All.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf-8 -*-
+'''
+FileName :  Check_If_All.py
+DateTime :  2020/09/27 16:38:50
+Author   :  Gong Yan 
+Version  :  1.0
+Contact  :  franck_gxu@outlook.com
+Desc     :  None
+'''
+
+# here put the import lib
+class Solution:
+    def kLengthApart(self, nums, k):
+        pass
---
+++ Consecutive_Characters.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+'''
+FileName :  Consecutive_Characters.py
+DateTime :  2020/09/27 16:51:23
+Author   :  Gong Yan 
+Version  :  1.0
+Contact  :  franck_gxu@outlook.com
+Desc     :  None
+'''
+
+# here put the import lib
+
+
+class Solution:
+    def maxPower(self, s):
+        pass
+
+        
